5fb8807436b8c5bb099c039070aa703ef2f9bdbd
Charles Eckman <charles@cceckman.com>
2016-12-10 00:42:29 -0800

    Update to user github.com/cceckman/bencher

--- benchmark/main.go
+++ benchmark/main.go
@@ -4,17 +4,19 @@ import (
 	"flag"
 	"fmt"
 	"github.com/cceckman/primes"
+	"github.com/cceckman/bencher"
 	"os"
-	"strings"
-	"testing"
-	"text/tabwriter"
 )
 
 var (
 	maxLevel = flag.Int("max_level", 5, "How far to run benchmarks: up to 1..1 with this many zeros in the middle.")
-	// human = flag.Bool("human", false, "Human-formatted output.") // TODO implement.
 	help = flag.Bool("help", false, "Display a usage message.")
 )
+
+type printInt int
+func (p printInt) String() string {
+	return fmt.Sprintf("%d", int(p))
+}
 
 // Benchmark the performance of different prime algorithms.
 // Output as TSV
@@ -29,45 +31,32 @@ func main() {
 		os.Exit(-1)
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-
 	levels := levelGen(*maxLevel)
 
-	// Output header
-	fmt.Fprint(w, "Function\tAlgorithm\tParameter\tIterations\tTotal Time (s)\tAverage Time (ns)\tAllocs\tBytes\n")
+	closures := make(bencher.Cases)
 
-	// First: bench PrimesUpTo, to get a set of primes to use
+	// benchmark PrimesUpTo, to get a set of primes to use
 	for name, primer := range primes.Implementations {
 		for _, level := range levels {
-			// create a benchmark that closes over level and primer
-			// Not running in parallel; OK to use those variables
-			b := func(b *testing.B) {
-				for i := 0; i < b.N; i++ {
-					c := make(chan int)
-					go primer.PrimesUpTo(level, c)
-					for _ = range c {
-					}
-				}
-			}
-			// Run the benchmark
-			result := testing.Benchmark(b)
-			// And write it out
+			// Copy locally.
+			primer := primer
+			level := level
 
-			fmt.Fprintln(w, strings.Join([]string{
-				"PrimesUpTo",
-				name,
-				fmt.Sprint(level),
-				fmt.Sprint(result.N),
-				fmt.Sprint(result.T),
-				fmt.Sprint(result.NsPerOp()),
-				result.MemString(),
-			}, "\t"))
+			caseName := fmt.Sprintf("%s: %s(%d)", "PrimesUpTo", name, level)
+
+			closure := func() fmt.Stringer {
+				c := make(chan int)
+				go primer.PrimesUpTo(level, c)
+				x := 0
+				for x = range c { }
+				return printInt(x)
+			}
+
+			closures[caseName] = bencher.Runnable(closure)
 		}
 	}
 
-	// Then: bench IsPrime
-
-	w.Flush()
+	bencher.AutoBenchmark(closures)
 }
 
 // Generate levels of stressyness; how many zeros we want.
