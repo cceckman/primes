bf339125cc2281282e8371dfb028c23aadfa80d4
Charles Eckman <charles@cceckman.com>
2016-12-10 01:11:29 -0800

    Add IsPrime benchmarking

--- benchmark/main.go
+++ benchmark/main.go
@@ -18,8 +18,12 @@ func (p printInt) String() string {
 	return fmt.Sprintf("%d", int(p))
 }
 
+type printBool bool
+func (p printBool) String() string {
+	return fmt.Sprintf("%t", bool(p))
+}
+
 // Benchmark the performance of different prime algorithms.
-// Output as TSV
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "%s: Benchmark performance of prime algorithms.\nUsage:\n", os.Args[0])
@@ -36,14 +40,16 @@ func main() {
 	closures := make(bencher.Cases)
 
 	// benchmark PrimesUpTo, to get a set of primes to use
+	primesForTesting := make(map[int]int)
+
 	for name, primer := range primes.Implementations {
 		for _, level := range levels {
-			// Copy locally.
+			// Override the names, for correctness.
 			primer := primer
 			level := level
 
+			// PrimesUpTo test
 			caseName := fmt.Sprintf("%s: %s(%d)", "PrimesUpTo", name, level)
-
 			closure := func() fmt.Stringer {
 				c := make(chan int)
 				go primer.PrimesUpTo(level, c)
@@ -51,10 +57,32 @@ func main() {
 				for x = range c { }
 				return printInt(x)
 			}
-
 			closures[caseName] = bencher.Runnable(closure)
+
+			// Make sure we have a set of primes to test. But this is still test-construction phase, so it
+			// won't count against the test itself.
+			if _, ok := primesForTesting[level]; !ok {
+				// Need to add primes for the corresponding test.
+				c := make(chan int)
+				go primer.PrimesUpTo(level, c)
+				x := 0
+				for x = range c { }
+				primesForTesting[level] = x
+			}
+
+			// And test IsPrime, with the max.
+
+			arg := primesForTesting[level]
+			caseName = fmt.Sprintf("%s: %s(%d)", "IsPrime", name, arg)
+			closure = func() fmt.Stringer {
+				return printBool(primer.IsPrime(arg))
+			}
+			closures[caseName] = bencher.Runnable(closure)
+
+
 		}
 	}
+	// Get a list of primes, for IsPrime tests
 
 	bencher.AutoBenchmark(closures)
 }
